7b82d7c2cacf8219e177fcaa8e1db32c395ae90b
Kevin Torres <ktorres99a@gmail.com>
2026-03-05 17:08:00 -0500

    fix: solucionar error 403 mediante la configuración de trustedOrigins en Better Auth

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -4,6 +4,11 @@ import { prisma } from '@/lib/prisma';
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL as string,
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL as string,
+    'https://prueba-tecnica-erp-prevalentware-ke.vercel.app'
+  ],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
