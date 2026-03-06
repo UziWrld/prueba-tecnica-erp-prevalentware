70af25458a22d113c73c07116e1eb1b7e512acb3
Kevin Torres <ktorres99a@gmail.com>
2026-03-06 10:42:13 -0500

    Prueba solución error Github

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
     try {
       await authClient.signIn.social({
         provider: 'github',
-        callbackURL: window.location.origin + '/',
+        callbackURL: 'https://prueba-tecnica-erp-prevalentware-ke.vercel.app/',
       });
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
@@ -49,8 +49,8 @@ export default function Login() {
           <Image
             src='/logo-prevalentware.png'
             alt='PrevalentWare'
-            width={260}
-            height={65}
+            width={300}
+            height={75}
             className='object-contain'
             priority
           />
